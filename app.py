62eb40a46ddfd102e4fe27eaaa3862b816f1ae03
Jon Chan <me@jonhmchan.com>
2020-03-22 12:54:26 -0700

    Updated data with better pokemon.json

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
+import json
 from flask import Flask, escape, request, jsonify, render_template
 
 app = Flask(__name__)
+current_id = 151
+
+with open('data/pokemon.json') as f:
+  data = json.load(f)
 
 @app.route('/')
 def hello():
     return render_template('index.html')
 
-@app.route('/api/messages', methods=['GET'])
-def api_messages_get():
-    data = [
-        {
-            "user": "Jon",
-            "message": "Hello!",
-            "created_at": "2020-01-01 00:00:00"
-        }
-    ]
-    return jsonify(data)
+@app.route('/api/pokemon', methods=['GET'])
+def api_pokemon_get():
+    return jsonify(data), 200
+
+@app.route('/api/pokemon/<int:id>', methods=['GET'])
+def api_pokemon_id_get(id):
+    for pokemon in data:
+        if pokemon.get("id") == id:
+            return jsonify(pokemon), 200
+    return jsonify({}), 404
+
+@app.route('/api/pokemon', methods=['POST'])
+def api_pokemon_post():
+    return "Yay!"
