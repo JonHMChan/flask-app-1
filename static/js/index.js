329861f2d944f2d9b74a56c766ca6e46e580671c
Jon Chan <me@jonhmchan.com>
2020-03-22 17:05:30 -0700

    Remove solution from repository

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,1 @@
 // Your code starts here
-$(document).ready(function() {
-    $.ajax({
-        method: "GET",
-        url: "/api/pokemon",
-        success: function(data) {
-            for (var i = 0; i < data.length; i++) {
-                var pokemon = data[i];
-                var $pokemon = $(`<a href="/${pokemon.id}"></a>`).addClass("pokemon");
-                var html = `
-                    <h2>${pokemon.name}</h2>
-                    <img class="pokemon-image" src=${pokemon.image_url} />
-                `;
-                $pokemon.append(html);
-                $(".pokemon-container").append($pokemon);
-            }
-        }
-    })
-})
